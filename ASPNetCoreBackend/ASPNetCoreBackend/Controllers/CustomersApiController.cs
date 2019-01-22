c99b79cdfe14275cd5a6896ff3acbae23775df0d
Kari Pursiainen <kari_pursiainen@hotmail.com>
2019-01-22 11:17:20 +0200

    Lisättu Asiakas API controller

---
+++ ASPNetCoreBackend/ASPNetCoreBackend/Controllers/CustomersApiController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNetCoreBackend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetCoreBackend.Controllers
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomersApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public List<Customers> GetAll()
+        {
+            NorthwindContext context = new NorthwindContext();
+            List<Customers> all = context.Customers.ToList();
+
+            return all;
+        }
+
+        [HttpGet]
+        [Route("{customerid}")]
+        public Customers GetSingle(string customerid)
+        {
+            NorthwindContext context = new NorthwindContext();
+
+            if (customerid != null)
+            {
+                Customers customer = context.Customers.Find(customerid);
+                return customer;
+            }
+            return null;
+        }
+
+
+        [HttpGet]
+        [Route("pvm")]
+        public string Päivämäärä()
+        {
+            string pvm = DateTime.Now.ToString();
+            return pvm;
+        }
+    }
+}
